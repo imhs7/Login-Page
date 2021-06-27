55b9ef43d6812b754a55a78ad8d10fdcb67c9e3a
Hemant Sharma <44858683+imhs7@users.noreply.github.com>
2021-06-27 13:26:35 +0530

    Add files via upload

---
+++ LoginSample/LoginSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LoginSample
+//
+//  Created by Hemant Sharma on 26/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var CodeplayonLabel: UILabel!
+    
+    @IBOutlet weak var LogInButton: UIButton!
+    
+    @IBAction func ForgetPasswordButton(_ sender: Any) {
+        print("Button Pressed")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+}
+
